c670bf724948d0e8066dabbed8a1ed8c11f44f0e
FrancisB <francis@madkudu.com>
2025-05-21 22:12:33 -0700

    clean up

--- content.js
+++
@@ -1,7 +0,0 @@
-const range = selection.getRangeAt(0);
-document.dispatchEvent(new CustomEvent('knotion-selection', {
-    detail: {
-        text: selection.toString(),
-        rect: range.getBoundingClientRect && range.getBoundingClientRect() || range.getClientRects()[0],
-    }
-})); 
